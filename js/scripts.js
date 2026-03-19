ad6c1b698575e6e7d936ec70a19dbbabf6673843
Ankush Gupta <152036385+ankushgupta29210@users.noreply.github.com>
2026-03-19 00:14:16 -0600

    Delete js/scripts.js

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll('.fade-in');
-
-window.addEventListener('scroll', () => {
-elements.forEach(el => {
-if(el.getBoundingClientRect().top < window.innerHeight - 100){
-el.classList.add('show');
-}
-});
-});
